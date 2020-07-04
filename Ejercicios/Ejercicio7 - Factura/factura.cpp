af69866ce4617e5c3beafa6f78e6b27f2dc88250
Bily Fernandez <cursosdecomputacion.net@gmail.com>
2020-07-04 16:37:01 -0600

    agregando ejercicio factura

---
+++ Ejercicios/Ejercicio7 - Factura/factura.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Datos de entrada
+    double subtotal = 0;
+    double total = 0;
+    double impuesto = 0.15;
+
+    cout << "Ingrese el subtotal de la factura: ";
+    cin >> subtotal;
+
+    // Proceso
+
+    total = subtotal + (subtotal * 0.15);
+
+    // Salida
+
+    cout << endl;
+    cout << "Total a pagar es: " << total;
+
+    return 0;
+}
