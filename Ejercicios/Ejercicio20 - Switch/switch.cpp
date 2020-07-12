6bbebe915ac213176535510f68ae03f466fbcf85
Bily Fernandez <cursosdecomputacion.net@gmail.com>
2020-07-12 13:39:46 -0600

    agregando ejercicio switch

---
+++ Ejercicios/Ejercicio20 - Switch/switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int opcion = 0;
+
+    cout << "Ingrese una opcion: ";
+    cin >> opcion;
+
+    switch (opcion)
+    {
+    case 1: 
+    {
+        cout << "Escogiste 1" << endl;
+        cout << "Segunda linea opcion 1" << endl;
+        break;
+    }
+    case 2:
+        cout << "Escogiste 2" << endl;
+        break;
+    case 3:
+        cout << "Escogiste 3" << endl;
+        break;            
+    default:
+        cout << "Ingrese una opcion entre 1 y 3" << endl;
+        break;
+    }
+
+    return 0;
+}
