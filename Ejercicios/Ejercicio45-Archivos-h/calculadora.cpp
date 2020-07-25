bf45055659460308fcd7101ecf924be2e670ed54
Bily Fernandez <cursosdecomputacion.net@gmail.com>
2020-07-25 16:11:12 -0600

    Agregando ejercicio con archivos h

---
+++ Ejercicios/Ejercicio45-Archivos-h/calculadora.cpp
@@ -0,0 +1,21 @@
+int sumar(int a, int b)
+{
+    return a + b;
+}
+
+int restar(int a, int b)
+{
+    return a - b;
+}
+
+int calcular(int a, int b, char operador)
+{
+    if (operador == '+') {
+        return sumar(a, b);
+    }
+    if (operador == '-') {
+        return restar(a, b);
+    }
+
+    throw "Operador no valido";
+}
